24bfb9161455b519432596b9c4b694d3fc6307ac
lh <1642228980@qq.com>
2022-12-13 23:46:06 +0800

    // zhushi

---
+++ IParams.java
@@ -0,0 +1,13 @@
+
+
+import java.util.ArrayList;
+ 
+interface IParams {
+    public int getBig();
+    // ��ȡ�а���
+    public int getMedium();
+    // ��ȡС������
+    public int getSmall();
+    // ��ȡ��ѧ���У����� [1 2 2 3] ��ʾ ���α�����ѧһ�����ѧ�����а�ѧ�����а�ѧ����С��ѧ��
+    public ArrayList<Integer> getPlanSignUp ();
+}
